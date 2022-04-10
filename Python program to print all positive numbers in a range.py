15c5bc0ab439c0b78cc2a8346d0e7d0c06d300e8
koushik240 <100862054+koushik240@users.noreply.github.com>
2022-04-10 14:19:54 +0530

    Add files via upload

---
+++ Python program to print all positive numbers in a range.py
@@ -0,0 +1,11 @@
+#Python program o print positive numbers in given range
+#list of numbers
+list1 = [12,-7, 5, 64,-14]
+list2 = [12,14,-95,3]
+#iterating each number in list
+for num in list1:
+  for num in list2:
+
+#checking condition
+   if num >= 0:
+    print(num, end = " ")
